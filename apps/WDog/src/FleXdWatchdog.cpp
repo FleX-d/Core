4013c457d53bb2822ae4946fbed9e887152e2427
IngBranislavPodkonicky <branislav.podkonicky@globallogic.com>
2018-08-08 10:46:59 +0200

    bug fixed

--- apps/WDog/src/FleXdWatchdog.cpp
+++ apps/WDog/src/FleXdWatchdog.cpp
@@ -24,116 +24,55 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 /*
- * File:   FleXdWatchdog.cpp
+ * File:   FleXdWatchdog.h
  * Author: Branislav Podkonicky
  *
  * Created on July 29, 2018, 09:45 AM
  */
 
-#include "FleXdWatchdog.h"
-#include <sys/inotify.h>
-#include <unistd.h>
+#ifndef FLEXDWATCHDOG_H
+#define FLEXDWATCHDOG_H
+
+#include "FleXdWatchdogEvent.h"
+#include <FleXdEpoll.h>
+#include <FleXdTimer.h>
 
 namespace flexd {
     namespace icl {
         namespace ipc {
             
-            FleXdWatchdog::FleXdWatchdog(FleXdEpoll& poller, int fd, std::string path)
-            : m_poller(poller),
-            m_fd(fd),
-            m_path(path),
-            m_timeout(60),
-            m_timer(m_poller, 1, 0, true, [&]{ this->onTimer(); }){
-                m_wd = inotify_add_watch(fd, path.c_str(), IN_DELETE);
-                if (m_wd == -1) {
-                    fprintf(stderr, "Cannot watch '%s'\n", path.c_str());
-                    perror("inotify_add_watch");
-                    exit(EXIT_FAILURE);
-                }
-                std::vector<std::string> pathVector;
-                pathVector.push_back(path);
-                
-                using namespace std::placeholders; 
-                m_event = std::make_unique<FleXdWatchdogEvent> (m_poller, m_fd, &m_wd, pathVector, 
-                        std::bind(&FleXdWatchdog::onEvent, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4));
-                m_event->init();
-                m_timer.start(); 
-                sendCreateMsg();
-            }
+            class FleXdWatchdog {  
             
-            FleXdWatchdog::~FleXdWatchdog() {
-                //inotify_rm_watch(m_fd, m_wd);
-                m_timer.stop();
-                sendDeleteMsg();
-            }
+            public:
+                explicit FleXdWatchdog(FleXdEpoll& poller, int fd, std::string path);
+                virtual ~FleXdWatchdog();
+               
+                std::string getPath();
+                int getTimeoutValue();
+                void setTimeoutValue(int value);
+                void resetTimer();
             
-            std::string FleXdWatchdog::getPath(){
-                return m_path;
-            }
+            private:
+                void sendCreateMsg();
+                void sendDeleteMsg();
+                void sendRestartMsg();
+                void onEvent(FleXdEpoll& poller, int fd, int * wd, std::vector<std::string> folders);
+                void onTimer();
             
-            void FleXdWatchdog::onTimer(){
-                this->setTimeoutValue(this->getTimeoutValue() - 1);
-                printf("FleXdWatchdog::onTimer() timeout value = %d \n", m_timeout);
-                if (this->m_timeout == 0){
-                    sendRestartMsg();
-                    this->m_timer.stop();
-                }
-            }
-            
-            int FleXdWatchdog::getTimeoutValue(){
-                return m_timeout;
-            }
-            
-            void FleXdWatchdog::setTimeoutValue(int value) {
-                m_timeout = value;
-            }
-            
-            void FleXdWatchdog::onEvent(FleXdEpoll& poller, int fd, int * wd, std::vector<std::string> folders) {             
-                char buf[4096]
-                        __attribute__((aligned(__alignof__(struct inotify_event))));
-                const struct inotify_event *event;
-                ssize_t len;
-                char *ptr;
-                
-                for (;;) {
-                    len = read(fd, buf, sizeof buf);
-
-                    if (len == -1 && errno != EAGAIN) {
-                        perror("read");
-                        exit(EXIT_FAILURE);
-                    }
-                    
-                    if (len <= 0)
-                        break;
-                    
-                    for (ptr = buf; ptr < buf + len;
-                            ptr += sizeof (struct inotify_event) +event->len) {
-                        event = (const struct inotify_event *) ptr;
-
-                        if (event->mask & IN_DELETE) {
-                            printf("IN_DELETE : ");
-                            this->m_timeout = 60;
-                        }
-                        if (event->mask & IN_MODIFY) {
-                            printf("IN_MODIFY: ");
-                            this->m_timeout = 60;
-                        }
-                    }
-                }
-            }
-            
-            void FleXdWatchdog::sendCreateMsg(){
-                printf("FleXdWatchdog::sendCreateMsg \n");
-            }
-            
-            void FleXdWatchdog::sendDeleteMsg(){
-                printf("FleXdWatchdog::sendDeleteMsg \n");
-            }
-            
-            void FleXdWatchdog::sendRestartMsg(){
-                printf("FleXdWatchdog::sendRestartMsg \n");
-            }
+            protected:
+                FleXdEpoll& m_poller;
+                int m_fd;
+                std::string m_path;
+                int m_wd;
+                int m_timeout;
+                FleXdTimer m_timer;
+                std::unique_ptr<FleXdWatchdogEvent> m_event;
+            };
             
         } // namespace ipc
     } // namespace icl
 } // namespace flexd
+
+
+#endif /* FLEXDWATCHDOG_H */
+
